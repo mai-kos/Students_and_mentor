078f17227ec16145f567c801c1339de9e6318af5
Konstantin <maykos94@gmail.com>
2022-12-01 23:49:01 +0300

    Added multiple methods to Student, Lecturer and Reviewer classes

--- Students_and_mentor.py
+++ Students_and_mentor.py
@@ -8,7 +8,25 @@ class Student:
         self.grades = {}
  
     def add_courses(self, course_name):
-        self.finished_courses.append(course_name) 
+        self.finished_courses.append(course_name)
+
+    def _get_average_hw_grade(self):
+        average_grade = sum(self.grades.values() / len(self.grades))
+        return average_grade
+
+    def rate_lecturer(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
+    def __str__(self):
+        res = f'Имя: {self.name}\n Фамилия: {self.surname}\n Средняя оценка за домашние задания: {self._get_average_hw_grade():.2f}\
+        \n Курсы в процессе изучения: {*self.courses_in_progress,}\n Завершенные курсы: {*self.finished_courses,}'
+        return res 
      
 class Mentor:
     def __init__(self, name, surname):
@@ -17,8 +35,23 @@ class Mentor:
         self.courses_attached = []
 
 class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades = {}
+
+    def _get_average_lecture_grade(self):
+        average_grade = sum(self.grades.values() / len(self.grades))
+        return average_grade
+
+    def __str__(self):
+        res = f'Имя: {self.name}\n Фамилия: {self.surname}\n Средняя оценка за лекции: {self._get_average_lecture_grade():.2f}'
+        return res
+
 
 class Reviewer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
@@ -27,6 +60,10 @@ class Reviewer(Mentor):
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
+
+    def __str__(self):
+        res = f'Имя: {self.name}\n Фамилия: {self.surname}'
+        return res
  
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
