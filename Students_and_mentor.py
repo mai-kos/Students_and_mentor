80f88ed8e9d2951231add54d6721c55d414f9b67
Konstantin <maykos94@gmail.com>
2022-12-01 21:37:29 +0300

    Added Lecturer and Reviewer classes

--- Students_and_mentor.py
+++ Students_and_mentor.py
@@ -8,15 +8,17 @@ class Student:
         self.grades = {}
  
     def add_courses(self, course_name):
-        self.finished_courses.append(course_name)   
- 
+        self.finished_courses.append(course_name) 
      
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached = []
-        
+
+class Lecturer(Mentor):
+
+class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
